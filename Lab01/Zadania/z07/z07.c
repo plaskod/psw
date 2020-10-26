3330daba6c87942e218d032fc6d14348ab8733cd
plaskod <davidblackfyre1@gmail.com>
2020-10-26 17:44:26 +0100

    poprawka nr 2 zadania 7

--- Lab01/Zadania/z07/z07.c
+++ Lab01/Zadania/z07/z07.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdbool.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #define MAX 1024
 
@@ -11,24 +11,16 @@ int main( int argc, char* argv[]){
 	int fd;
 	fd = open(argv[1], O_RDWR);
 
-	bool tekst= true;
 	char x;
 	while(read(fd, &x, 1)>0){
-		if (isascii(x)!=true){
-			tekst=false;
-			break;
+		if (isascii(x)!=1){
+		  char text[]= "Plik nie jest tekstowy \n"; 
+		  write(2, text, sizeof(text));
+		  exit(0); 	
 		}
 	}
-
-
-	if(tekst){
-		 char text[]= "Plik tekstowy \n"; 
-		 write(2, text ,sizeof(text));
-	}
-	else{
-		 char text[]="Plik nie tekstowy \n";
+		 char text[]="Plik jest tekstowy \n";
 		 write(2, text, sizeof(text));
-	}
 
 	return 0;
 }
