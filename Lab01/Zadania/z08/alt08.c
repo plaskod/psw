2974c3c3bd802c504afce3f645a42f490e897984
plaskod <davidblackfyre1@gmail.com>
2020-10-26 22:21:24 +0100

    zad8 done

--- Lab01/Zadania/z08/alt08.c
+++ Lab01/Zadania/z08/alt08.c
@@ -10,32 +10,56 @@ void terminate(char *msg);
 
 int main(int argc, char* argv[]){
 
-	if(argc <= 1) terminate("Za malo argumentow");
-	int fd;
-	if((fd= open(argv[1], O_RDWR))<0) terminate("Blad przy otwarciu pliku");
-	char x;
-	while((read(fd, &x, 1)>0)){
-		int tmp=x;	
-		if(tmp >= 65 && tmp <= 90){
-			tmp+=32;
-		}
+	if(argc <= 0) terminate("Za malo argumentow");
+	else if(argc==1){
+		char x;
+		char buf[MAX];
+		int iter=0;
+		while((read(1,&x,1)>0)){
+			int tmp=x;
+			if(tmp >= 65 && tmp <= 90){
+				tmp+=32;
+			}
+			else if(tmp >= 97 && tmp <=122){
+				tmp-=32;
+			}
+			if (tmp=='\n'){
+				buf[iter]='\n';
+			}
+			else{
+				buf[iter]=tmp;
+			}
+			iter++;
 
-		else if(tmp >= 97 && tmp <= 122){
-			tmp-=32;
-		
 		}
-
-		lseek(fd, -1, SEEK_CUR);
-		if(tmp == '\n'){
-			write(fd, "\n", 1);
-		}
-		else{	
-			char temp=tmp;
-			write(fd,&temp,1);
-		}
-	
+		write(1,"\n",1);		
+		write(1,buf,iter);		
 	}
-	if(close(fd)<0) terminate("Blad przy zamknieciu pliku");
+	else{
+		int fd;
+		if((fd= open(argv[1], O_RDWR))<0) terminate("Blad przy otwarciu pliku");
+		char x;
+		while((read(fd, &x, 1)>0)){
+			int tmp=x;	
+			if(tmp >= 65 && tmp <= 90){
+				tmp+=32;
+			}
+
+			else if(tmp >= 97 && tmp <= 122){
+				tmp-=32;
+			}
+
+			lseek(fd, -1, SEEK_CUR);
+			if(tmp == '\n'){
+				write(fd,"\n",1);
+			}
+			else{	
+				char temp=tmp;
+				write(fd,&temp,1);
+			}
+		}
+		if(close(fd)<0) terminate("Blad przy zamknieciu pliku");
+	}
 	return 0;
 }
 
