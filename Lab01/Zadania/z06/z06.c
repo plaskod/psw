10b843f749a99fa8fbc801967a2cef3a90e4f537
plaskod <davidblackfyre1@gmail.com>
2020-10-28 20:19:25 +0100

    zad 06

---
+++ Lab01/Zadania/z06/z06.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define MAX 1024		
+
+
+void terminate(char *msg){
+
+	if(errno!=0){
+		perror(msg);
+	}
+
+	else{
+		fprintf(stderr, "ERROR: %s\n", msg);
+	}
+	exit(1);
+}
+int main(int argc, char* argv[]){
+
+	if(argc <= 1) terminate("Za malo argumentow");
+	int fd,fd2;
+	if((fd= open(argv[1], O_RDONLY))<0) terminate("Blad przy otwarciu pliku nr. 1");	
+	if((fd2= open(argv[2], O_WRONLY))<0) terminate("Blad przy otwarciu pliku nr. 2");
+	
+	lseek(fd,-10,SEEK_END);
+	char z;
+	for(int i=0; i<10; i++){
+		read(fd,&z,1);
+		write(fd2,&z,1);
+	}
+	write(fd2,"\n",1);
+	
+	char buf[MAX];
+	int iter=0;	
+	int num_of_words=0;
+	int filesize;
+	filesize=lseek(fd,0,SEEK_END);
+	
+	char x;	
+	for(int i=filesize -1; i>=0; i--){
+		lseek(fd,i,SEEK_SET);
+		read(fd, &x,1);
+		
+		if(num_of_words == 9 && x==' '){
+			num_of_words++;
+			break;
+		}	
+		else if(x==' '){
+			num_of_words++;
+			buf[iter]=' ';
+			iter++;
+		}
+		else{
+			buf[iter]=x;
+			iter++;
+		}
+	}
+
+	for(int j=iter; j>=0; j--){
+			
+		write(fd2,&buf[j],1);
+	}
+	
+	write(fd2,"\n",1);
+	
+
+
+
+
+
+	if(close(fd)<0) terminate("Blad przy zamknieciu pliku nr 1");
+	if(close(fd2)<0) terminate("Blad przy zamknieciu pliku nr 2");
+	return 0;
+}
+
