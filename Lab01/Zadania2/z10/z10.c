42f17893fadf43442df44af6a5eefc32885bad72
plaskod <davidblackfyre1@gmail.com>
2020-10-29 17:51:37 +0100

    zad10 done

--- Lab01/Zadania2/z10/z10.c
+++ Lab01/Zadania2/z10/z10.c
@@ -31,7 +31,29 @@ int main(int argc, char* argv[]){
 		}
 		printf("\nIlosc podanych slow to: %d\n",word_cnt); 
 	}
-	
+	else{
+		int fd;
+		if((fd=open(argv[1], O_RDONLY))<0) terminate("Blad w otwieraniu pliku");
+		char x;
+		int word_cnt=0;
+		int was_word=0;
+		while((read(fd,&x,1)>0)){
+			int tmp=x;
+			if(((tmp >=48 && tmp <=57)||(tmp >= 65 && tmp <= 90)||(tmp >= 97 && tmp <= 122)||(tmp == 95))&&(was_word==0)) {	
+				word_cnt++;
+				was_word=1;
+			}
+			else if(((tmp >=48 && tmp <=57)||(tmp >= 65 && tmp <= 90)||(tmp >= 97 && tmp <= 122)||(tmp == 95))) {	
+				continue;
+			}
+			else{
+				was_word=0;
+			}
+
+		}
+		if(close(fd)<0) terminate("Blad przy zamykaniu pliku");
+		printf("\nIlosc slow w pliku: %s wynosi: %d\n",argv[1],word_cnt);
+	}	
 	return 0;
 }
 
